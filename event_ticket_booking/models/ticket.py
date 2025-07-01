3e71aa410234fe6a25526517c2cdc95e3653cab4
CodeCaret <codecaret@gmail.com>
2025-07-01 12:57:27 +0530

    Added Ticket model

---
+++ event_ticket_booking/models/ticket.py
@@ -0,0 +1,6 @@
+class Ticket:
+    def __init__(self, user_id, event_id, price, booking_time):
+        self.user_id = user_id
+        self.event_id = event_id
+        self.price = price
+        self.booking_time = booking_time
