857c7b78ec5693cefcdaf698c5b9044affc15e56
CodeCaret <codecaret@gmail.com>
2025-07-01 12:55:18 +0530

    Added Event model

---
+++ event_ticket_booking/models/event.py
@@ -0,0 +1,6 @@
+class Event:
+    def __init__(self, title, location, date_time, total_seats):
+        self.title = title
+        self.location = location
+        self.date_time = date_time
+        self.total_seats = total_seats
