77ece033b91ca6c5517f4f36e7971f2f1f167b87
CodeCaret <codecaret@gmail.com>
2025-07-01 13:47:30 +0530

    Added UserNotFoundError and IncorrectPasswordError

--- event_ticket_booking/utils/exceptions.py
+++ event_ticket_booking/utils/exceptions.py
@@ -6,3 +6,11 @@ class BookingError(Exception):
 class UserAlreadyExistsError(BookingError):
     def __init__(self, message = 'User with this email already exists'):
         super().__init__(message)
+
+class UserNotFoundError(BookingError):
+    def __init__(self, message = "User with this email doesn't exists"):
+        super().__init__(message)
+
+class IncorrectPasswordError(BookingError):
+    def __init__(self, message = "Incorrect Password"):
+        super().__init__(message)
